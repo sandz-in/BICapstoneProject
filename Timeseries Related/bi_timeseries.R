292cd0f847f4ada42179676f97a6077145ae2803
samrudhisharma <samrudhi.s21@gmail.com>
2016-04-16 23:59:01 -0400

    change

--- Timeseries Related/bi_timeseries.R
+++ Timeseries Related/bi_timeseries.R
@@ -1,17 +1,18 @@
 getwd()
-births <- read.table("/Users/samrudhi/Downloads/island.csv")
+births <- read.table("path_to_csv", sep = ",")
 
-birthstimeseries <- ts(births, frequency=12, start=c(2000,1))
-birthstimeseries
+dummy = as.vector(t(births))
+birthstimeseries <- ts(dummy,start=c(2000, 1), end=c(2014, 12), frequency=12)
+
 plot(birthstimeseries)
 birthstimeseries.stl = stl(birthstimeseries, s.window="periodic")
 plot(birthstimeseries.stl)
 
 plot(birthstimeseries, xaxt = "n")
 tsp = attributes(birthstimeseries)$tsp
-dates = seq(as.Date("2001-01-01"), by = "month", along = birthstimeseries)
+dates = seq(as.Date("2000-01-01"), by = "month", along = birthstimeseries)
 axis(1, at = seq(tsp[1], tsp[2], along = birthstimeseries), labels = format(dates, "%Y-%m"))
-df = data.frame(date = seq(as.POSIXct("2001-01-01"), by = "month", length.out = 24), pcp = rnorm(24))
+df = data.frame(date = seq(as.POSIXct("2000-01-01"), by = "month", length.out = 24), pcp = rnorm(24))
 library(ggplot2)
 library(scales)
 p = ggplot(data = df, aes(x = date, y = pcp)) + geom_line()
