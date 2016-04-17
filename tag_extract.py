1f339fe50dcca8a64937610da4974c05b89b8585
Jigar <jigar.shar24@gmail.com>
2016-04-16 22:54:27 -0400

    Extract User Tags

---
+++ tag_extract.py
@@ -0,0 +1,22 @@
+import csv
+import re
+import operator
+from collections import defaultdict
+
+tags = {}
+tags = defaultdict(int)
+with open("samrx") as tsv:
+	for line in csv.reader(tsv, dialect="excel-tab"):
+		if line[10]!="":
+			for val in line[8].split(","):
+				tags[val] += 1
+
+sorted_x = sorted(tags.items(), key= operator.itemgetter(1),reverse=True)
+a = open('tags.txt','wb')
+for key in sorted_x:
+	a.write(str(key))
+	#a.write("-->")
+	#a.write(str(tags.get(key)))
+	a.write("\n")
+		
+		
