12e258cafb95414bb6e5fde8aad67c492dfc9402
Jigar <jigar.shar24@gmail.com>
2016-04-16 23:57:23 -0400

    generate csv for all tags

--- Timeseries Related/date_Extract.py
+++ Timeseries Related/date_Extract.py
@@ -9,43 +9,42 @@ import csv
 client =  MongoClient()
 db = client['flickr']
 
+tags = ['beach', 'church', 'mountains', 'sculpture','landscape', 'france', 'lake', 'island', 'temple', 'sunset',
+'snow', 'hiking', 'river', 'forest', 'zoo', 'bridge', 'wildlife', 'castle']
+
 image_collection = db['image_dataset_timeseries']
 
-regx = re.compile("france", re.IGNORECASE)
+for tag in tags:
+	regx = re.compile(tag, re.IGNORECASE)
+	cursor = image_collection.find({"field8": regx})
+
+	dates = []
+	print cursor.count()
+
+	for hit in cursor:
+		print hit['field3']
+		if ('null' not in hit['field3']) :
+			dates.append(datetime.strptime(hit['field3'], '%Y-%m-%d %H:%M:%S.%f'))
+
+	dates.sort()
+
+	pickle.dump(dates, open('temp1.txt', 'wb'))
+
+	dd = pickle.load(open('temp1.txt', 'rb'))
+
+	final_dict = defaultdict(int)
+
+	for date in dd:
+		final_dict[(date.year,date.month)]+=1
 
 
+	years = range(2000,2015)
+	dump_list = []
 
-cursor = image_collection.find({"field8": regx})
+	for year in years:
+		for i in range(1,13):
+			dump_list.append(final_dict.get((year,i),0))
 
-dates = []
-print cursor.count()
-
-for hit in cursor:
-	print hit['field3']
-	#if 
-	if ('null' not in hit['field3']) :
-		dates.append(datetime.strptime(hit['field3'], '%Y-%m-%d %H:%M:%S.%f'))
-
-
-dates.sort()
-
-pickle.dump(dates, open('temp1.txt', 'wb'))
-
-dd = pickle.load(open('temp1.txt', 'rb'))
-
-final_dict = defaultdict(int)
-
-for date in dd:
-	final_dict[(date.year,date.month)]+=1
-
-
-years = range(2000,2015)
-dump_list = []
-
-for year in years:
-	for i in range(1,13):
-		dump_list.append(final_dict.get((year,i),0))
-
-ff = open('date.csv','wb')
-wr =  csv.writer(ff,quoting = csv.QUOTE_ALL)
-wr.writerow(dump_list)
+	ff = open(tag+'.csv','wb')
+	wr =  csv.writer(ff,quoting = csv.QUOTE_ALL)
+	wr.writerow(dump_list)
