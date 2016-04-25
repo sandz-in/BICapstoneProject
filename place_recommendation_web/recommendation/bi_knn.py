1306abf131959aa3202955eb10106c76a1c68cc5
ZealGanatra <ganatrazeal@gmail.com>
2016-04-25 18:09:57 -0400

    added comments

--- place_recommendation_web/recommendation/bi_knn.py
+++ place_recommendation_web/recommendation/bi_knn.py
@@ -15,27 +15,25 @@ train_collection = db["image_dataset"]
 
 
 def knn_recommender(url_image):
+    # Files open and loaded in read mode
     category_feature_matrix = pickle.load(open('bi_feature_matrix/category_feature_matrix', 'rb'))
     global_image_mapping = pickle.load(open('bi_feature_matrix/global_image_mapping', 'rb'))
+    # we create a model using 10 nearest neighbour 
     image_model = NearestNeighbors(n_neighbors=10, algorithm="auto").fit(category_feature_matrix)
+    # generate features and probabilities of that features
     features_image, probs_image = generate_tags(url_image)
-
-    # features_image = [u'bridge', u'water', u'river', u'reflection', u'no person', u'sunset', u'sky', u'travel',
-    #                   u'evening', u'architecture', u'dawn', u'city', u'dusk', u'light', u'suspension', u'urban',
-    #                   u'landscape', u'transportation system', u'suspension bridge', u'lake']
-    #
-    # probs_image = [0.9987363815307617, 0.9966945648193359, 0.9950028657913208, 0.9752582311630249, 0.9750866889953613,
-    #                0.9703925848007202, 0.9699936509132385, 0.9686242341995239, 0.9574745893478394, 0.949645459651947,
-    #                0.9459954500198364, 0.9424264430999756, 0.9072239398956299, 0.898352324962616, 0.8937841653823853,
-    #                0.8838391304016113, 0.8808287382125854, 0.8789186477661133, 0.8749411106109619, 0.8702283501625061]
 
     result = []
     feature_image_row_vector = np.zeros((1, len(features)))
+    # iterates over each feature of the image
     for j in range(len(features_image)):
+        # if the image feature exists in the predetermined features
         feature = features_image[j]
         if feature in features:
             feature_index = features.index(feature)
+            # takes in the probability of that feature
             feature_image_row_vector[0, feature_index] = probs_image[j]
+    #get images and distance of the k nearest image        
     distance_near_image, images_near = image_model.kneighbors(feature_image_row_vector)
     for image_near in images_near[0]:
         id_image = global_image_mapping[image_near]
@@ -44,6 +42,3 @@ def knn_recommender(url_image):
             print data["field14"]
             result.append(data["field14"])
     return result
-
-#
-# knn_recommender()
--- place_recommendation_web/recommendation/clarifie.py
+++ place_recommendation_web/recommendation/clarifie.py
@@ -1,6 +1,6 @@
 from clarifai.client import ClarifaiApi
 
-__author__ = 'sandz'
+#credentials to access the Clarifai API
 clarifai_api = ClarifaiApi(app_id="kqwTnQ7CEHM1v3W_nO4Qi0A6k5VjKmzINOKEDG9d",
                            app_secret="ZCdFchZrIfS0nPMD64mG_keGcAnIPHQnn7dVxX1K")
 
@@ -9,7 +9,10 @@ clarifai_api = ClarifaiApi(app_id="kqwTnQ7CEHM1v3W_nO4Qi0A6k5VjKmzINOKEDG9d",
 
 
 def generate_tags(url_image):
+	# url_image is the url of the image that need to be tagged. 
+	# the tags are then stored in result in JSON format.
     result = clarifai_api.tag_image_urls(url_image)
+    # result is a nested JSON, from which we can now get classes and probabilities
     tags_result = result[u'results'][0]["result"]["tag"]
     tags = tags_result["classes"]
     probs = tags_result["probs"]
